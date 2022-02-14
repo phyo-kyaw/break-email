d8a2be43ba37d0596fdbef8ec9aa414e0fc0de7f
phyo-kyaw <phy0ky6w@gmail.com>
2022-02-14 16:13:12 +1100

    kafka id

--- src/main/java/org/thebreak/breakemail/event/BookedNotificationEvent.java
+++ src/main/java/org/thebreak/breakemail/event/BookedNotificationEvent.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+//import org.springframework.data.annotation.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
 public class BookedNotificationEvent {
+    //@Id
+    private String id;
     private String bookerEmail;
     private String bookerName;
     private int version;
