2f5cd065b38068567acfb77661ef4c61bc7e46ae
phyo-kyaw <phy0ky6w@gmail.com>
2022-02-18 17:40:41 +1100

    kafka upd

---
+++ src/main/java/org/thebreak/breakemail/config/EventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package org.thebreak.breakemail.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class EventsConsumerConfig {
+}
